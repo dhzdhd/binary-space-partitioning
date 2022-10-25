6380f27e8aa691bb1abd9b0c2038b8743f6bb6e4
dhzdhd <ragas5432100@gmail.com>
2022-10-25 21:51:39 +0530

    Improve running of executable command

--- run.ps1
+++ run.ps1
@@ -1,20 +1,16 @@
 $Folder = 'build'
-$Executable = 'build/bsp.exe'
 
 if (Test-Path -Path $Folder) {
     Set-Location build;
 }
 else {
-    mkdir build; Set-Location build;
+    mkdir build
+    Set-Location build
 }
 
 cmake .. -G "MinGW Makefiles"
 cmake --build .
 Set-Location ..
 
-if (Test-Path -Path $Executable) {
-    ./build/bsp.exe
-}
-else {
-    Write-Output "Check build folder for executable and run it"
-}
+$Executable = Get-ChildItem -Path bsp.exe -Recurse -File | ForEach-Object { $_.FullName }
+& $Executable
