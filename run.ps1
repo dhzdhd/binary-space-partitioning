273eeef0a38e9eb54de6f00661487b112b152694
dhzdhd <ragas5432100@gmail.com>
2022-10-25 21:40:35 +0530

    Update ps file to not error out

--- run.ps1
+++ run.ps1
@@ -1,1 +1,20 @@
-cd build; cmake .. -G "MinGW Makefiles" ; cmake --build .; cd ..
+$Folder = 'build'
+$Executable = 'build/bsp.exe'
+
+if (Test-Path -Path $Folder) {
+    Set-Location build;
+}
+else {
+    mkdir build; Set-Location build;
+}
+
+cmake .. -G "MinGW Makefiles"
+cmake --build .
+Set-Location ..
+
+if (Test-Path -Path $Executable) {
+    ./build/bsp.exe
+}
+else {
+    Write-Output "Check build folder for executable and run it"
+}
